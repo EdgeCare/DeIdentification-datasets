ceda76cc25375aeba8e7aab4a7ec649e04ec3c01
Thejan Weerasekara <92569392+ThejanB@users.noreply.github.com>
2024-12-23 18:20:57 +0530

    1-35 data added

--- test.py
+++ test.py
@@ -2,10 +2,12 @@
 data = [
     
 ]
+
 for datapoint in data:
     datapoint["text"] = datapoint["text"].replace(" ", "_")
 
-    print(datapoint["text"], " ", len(datapoint["spans"]))
+    print("id: ", datapoint["meta"]["note_id"], "| spans", len(datapoint["spans"]))
+    print(datapoint["text"])
     for span in datapoint["spans"]:
-        print(span["label"],"\t", datapoint["text"][span["start"]:span["end"]])
+        print("\t", span["label"],"\t", datapoint["text"][span["start"]:span["end"]])
     print()
