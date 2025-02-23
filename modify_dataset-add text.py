b403e4d1f0c1dc6ffb9d391f2042e85e90820be1
Thejan Weerasekara <92569392+ThejanB@users.noreply.github.com>
2025-02-24 00:48:26 +0530

    Added new sentences

--- modify_dataset-add text.py
+++ modify_dataset-add text.py
@@ -47,3 +47,20 @@ if ADD_START_END:
     print("Added start and end to spans in the dataset")
 
 print("Updated dataset saved as 'updated_dataset.json'")
+
+# # label counts in the dataset
+with open('all_data.json', 'r', encoding='utf-8') as f:
+    data = json.load(f)
+
+    label_count = {}
+    for row in data:
+        for span in row['spans']:
+            label = span['label']
+            if label in label_count:
+                label_count[label] += 1
+            else:
+                label_count[label] = 1
+
+with open('label_count.json', 'w') as f:
+    json.dump(label_count, f, indent=4)
+    print('label count saved to label_count.json')
